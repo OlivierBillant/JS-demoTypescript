e38a04bf37ad1241b929bdc274eed526b52c4d2c
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-12 11:18:50 +0200

    ADD : creation class

--- demoTS2.js
+++ demoTS2.js
@@ -1,2 +1,15 @@
 console.log("Hello from TS !");
-// test
+var FigureGeometrique = /** @class */ (function () {
+    function FigureGeometrique(couleur) {
+        this.couleur = couleur;
+    }
+    FigureGeometrique.prototype.getCouleur = function () {
+        return this.couleur;
+    };
+    FigureGeometrique.prototype.setCouleur = function (couleur) {
+        this.couleur = couleur;
+    };
+    return FigureGeometrique;
+}());
+var figure = new FigureGeometrique("rouge");
+console.log(figure.getCouleur());
--- demoTS2.ts
+++ demoTS2.ts
@@ -1,2 +1,21 @@
 console.log("Hello from TS !");
 
+class FigureGeometrique {
+    private couleur:string;
+
+    constructor(couleur:string) {
+    this.couleur = couleur;
+    }
+
+    getCouleur():string {
+        return this.couleur;
+    }
+
+    setCouleur(couleur: string){
+        this.couleur = couleur;
+    }
+}
+
+let figure = new FigureGeometrique("rouge");
+
+console.log(figure.getCouleur());
