34de2ed4a595e4e07db887a66d11b7ed1a4bc17e
Olivier Billant <99239307+OlivierBillant@users.noreply.github.com>
2022-05-12 11:43:09 +0200

    CHANGE : ajout abstraction de classes et de methodes

--- demoTS2.js
+++ demoTS2.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 console.log("Hello from TS !");
 var FigureGeometrique = /** @class */ (function () {
     function FigureGeometrique(couleur) {
@@ -11,5 +26,29 @@ var FigureGeometrique = /** @class */ (function () {
     };
     return FigureGeometrique;
 }());
-var figure = new FigureGeometrique("rouge");
-console.log(figure.getCouleur());
+var Carre = /** @class */ (function (_super) {
+    __extends(Carre, _super);
+    function Carre(couleur, cote) {
+        var _this = _super.call(this, couleur) || this;
+        _this.cote = cote;
+        return _this;
+    }
+    Carre.prototype.getNombreCote = function () {
+        return this.cote;
+    };
+    Carre.prototype.calculPerimetre = function () {
+        return 4 * this.cote;
+    };
+    Carre.prototype.calculAire = function () {
+        return this.cote * this.cote;
+    };
+    return Carre;
+}(FigureGeometrique));
+//Ayant passe la classe Figure en abstract, on ne peut plus l'instancier
+// let figure = new FigureGeometrique("rouge");
+// console.log(figure.getCouleur());
+var nouveauCarre = new Carre("bleu", 3);
+console.log("Le périmètre du carré est de : "
+    + nouveauCarre.calculPerimetre() + " et sa couleur est : "
+    + nouveauCarre.getCouleur() + " et sa surface est de "
+    + nouveauCarre.calculAire());
--- demoTS2.ts
+++ demoTS2.ts
@@ -1,6 +1,6 @@
 console.log("Hello from TS !");
 
-class FigureGeometrique {
+abstract class FigureGeometrique {
     private couleur:string;
 
     constructor(couleur:string) {
@@ -14,8 +14,37 @@ class FigureGeometrique {
     setCouleur(couleur: string){
         this.couleur = couleur;
     }
+
+    abstract calculAire():number;
 }
 
-let figure = new FigureGeometrique("rouge");
+class Carre extends FigureGeometrique{
+    private cote:number;
 
-console.log(figure.getCouleur());
+    constructor(couleur:string, cote:number) {
+        super(couleur);
+        this.cote = cote;
+    }
+
+    getNombreCote():number{
+        return this.cote;
+    }
+
+    calculPerimetre():number{
+        return 4*this.cote;
+    }
+
+    calculAire(): number {
+        return this.cote*this.cote;
+    }
+}
+
+//Ayant passe la classe Figure en abstract, on ne peut plus l'instancier
+// let figure = new FigureGeometrique("rouge");
+// console.log(figure.getCouleur());
+
+let nouveauCarre = new Carre("bleu", 3);
+console.log("Le périmètre du carré est de : "
+            +nouveauCarre.calculPerimetre()+" et sa couleur est : "
+            +nouveauCarre.getCouleur()+" et sa surface est de "
+            +nouveauCarre.calculAire());
